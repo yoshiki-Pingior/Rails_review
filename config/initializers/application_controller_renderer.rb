ad219e7ba26601d35e1a295d6d557dea8f1fabb8
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-07-15 12:12:06 +0000

    [practice]rails_base

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9b8ea8351a5dbb82423223bb1ded096a9dd04d8665f67bc71448562196ae9e18ce4c12443e878e669c920df4296bd7f15d061a2b6915c6ed461dfcd8a1c1a918'
